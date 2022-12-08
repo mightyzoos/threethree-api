b2d809774f17efb240a749ca33fe4394b703f4f3
Dan Zhu <danzhu54@gmail.com>
2022-12-08 15:23:48 -0800

    Update api call to limit response

--- routes/yelp.js
+++ routes/yelp.js
@@ -6,7 +6,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const router = Router();
-const sdk = api('@yelp-developers/v1.0#a2embhlb4wb4ot');
+const sdk = api('@yelp-developers/v1.0');
+sdk.server(process.env.YELP_API_URL);
 sdk.auth(`Bearer ${process.env.YELP_API_KEY}`);
 
 router.get('/autocomplete', (req, res) => {
@@ -22,17 +23,23 @@ router.get('/autocomplete', (req, res) => {
 });
 
 router.get('/business', (req, res) => {
-    const { term, latitude, longitude, price, open_now, open_at, sort_by } = url.parse(req.url, true).query;
+    const { term, latitude, longitude, price, open_now, open_at, sort_by, limit, offset } = url.parse(req.url, true).query;
 
-    return sdk.v3_business_search({
+    const options = {
         term: term,
         longitude: longitude,
         latitude: latitude,
-        price: price,
-        open_now: open_now,
-        open_at: open_at,
-        sort_by: sort_by
-    })
+        sort_by: sort_by ?? 'best_match',
+        limit: limit ?? 25
+    };
+
+    if (price) options.price = price;
+    if (open_now) options.open_now = open_now;
+    if (open_at) options.open_at = open_at;
+    if (limit) options.limit = limit;
+    if (offset) options.offset = offset;
+
+    return sdk.v3_business_search(options)
         .then((data) => res.send(data))
         .catch((err) => console.log(err));
 });
