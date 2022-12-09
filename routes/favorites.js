3e51c08fa0f09512a7fcce2ef8a85553625a1afc
Dan Zhu <danzhu54@gmail.com>
2022-12-09 02:03:00 -0800

    Expand favorites API

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,11 +2,32 @@ import { Router } from 'express';
 
 const router = Router();
 
-let list = []
+let list = [];
+let lastChanged = 0;
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     try {
         res.send(JSON.stringify(list));
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/lastChanged', (_, res) => {
+    try {
+        res.send(JSON.stringify({ lastChanged: lastChanged }));
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/reset', (_, res) => {
+    try {
+        list = [];
+        lastChanged = 0;
+        res.sendStatus(200);
     }
     catch (err) {
         console.log(err);
@@ -16,6 +37,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     try {
         list = JSON.parse(JSON.stringify(req.body));
+        lastChanged = Date.now();
         res.sendStatus(200);
     }
     catch (err) {
